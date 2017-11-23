e85868079f2219693eda8f53ce46ff46072bbf37
pathikrit-bhattacharya <33680586+pathikrit-bhattacharya@users.noreply.github.com>
2017-11-23 03:31:45 -0500

    Update util_mcmc.f90

--- MCMC_general_purpose/util_mcmc.f90
+++ MCMC_general_purpose/util_mcmc.f90
@@ -1,12 +1,11 @@
 module util_mcmc
 
-use global_fric
+use global_data
 use nrtype
 use nrutil
 use nr_bsstep
 use utilities
 use matutils
-use parameters
 use randn_gen
 use random_gen
 
@@ -15,143 +14,13 @@ implicit none
 
 contains 
 
-subroutine combined_model(p_acc)
 
-	  real(8), dimension(:,:), intent(in) :: p_acc
-	  real(8), dimension(:), allocatable  :: x_f, x_lp, t, v_lp, tau, slipv, v_f
-	  real(8), dimension(:), allocatable  :: state, sliprate, rand, state_av, fmu, sliprate_av, slipv_av, tau_av
-	  real(8)                             :: t1
-	  character                           :: ln
-	  character, dimension(29)            :: fm
-	  integer                             :: i, Nav, sz
-	  integer, dimension(:), allocatable  :: collect
-	  
-	  	allocate(x_f(Nrange),tau(Nrange),x_lp(Nrange),t(Nrange),v_lp(Nrange))
-	  	allocate(slipv(Nrange),sliprate(Nrange),state(Nrange),fmu(Nrange),v_f(Nrange))
-	  	allocate(slipv_av(Nrange),sliprate_av(Nrange),state_av(Nrange),tau_av(Nrange))
-	  	Nav = 100
-	  	allocate(rand(Nav))
-	  	allocate(collect(Nav))
-	  	call init_random_seed()
-		call random_number(rand)
-	  	collect=1+int(dble(size(p_acc,2))*rand)
-		open(unit=112,file=mcmcavout,STATUS='REPLACE')
-		open(unit=113,file=fitdata,STATUS='OLD') 
-		do i = 1,Nrange
-			read(113,'(7f18.6)') x_lp(i), t(i), fmu(i), v_lp(i), x_f(i), v_f(i), t1
-		enddo	
-		close(113)
-		
-		slipv_av     = 0.d0
-		sliprate_av = 0.d0
-		state_av    = 0.d0
-		tau_av      = 0.d0
-		
-		do i = 1, Nav
-			call fmodel(p_acc(:,i), tau, slipv, sliprate, state, x_f, x_lp, t, v_lp)
-			slipv_av     = slipv_av + slipv
-			sliprate_av = sliprate_av + sliprate
-			state_av    = state_av + state
-			tau_av      = tau_av + tau
-		enddo
-		slipv_av     = slipv_av/Nav
-		sliprate_av = sliprate_av/Nav
-		state_av    = state_av/Nav
-		tau_av      = tau_av/Nav
-		do i = 1,Nrange
-			write(112,'(2f14.5,e14.5,3f14.5,e14.5,i6,3e14.5)')t(i),x_f(i), &
-		  	sliprate_av(i),slipv_av(i),tau_av(i),fmu(i),sliprate(i)*state_av(i)/dc,i,v_f(i), t(i), x_lp(i)
-		enddo
-end subroutine combined_model
-
-!------------------------------------------------------------------------------------------------------------------------!
-!------------------------------------------------------------------------------------------------------------------------!
-
-subroutine fmodel(rnstry, tau, slipv, sliprate, state, x_f, x_lp, t, v_lp)
-
-! This version has no sn! because output is in terms of f.
-! stiffk/sn has been changed to stiffk
-! The velocity used to update is the change in l.p. disp. from the previous line 
-!   divided by the time since the previous line.
-!
-	implicit none
-      	  real(DP), dimension(:), intent(in) :: rnstry
-	  real(8), dimension(:), intent(in)  :: x_f, x_lp, t, v_lp
-	  real(8), dimension(:), intent(inout) :: tau, slipv, sliprate, state
-      	  real(8), dimension(:), allocatable :: rnstry_true
-	  integer, parameter                 :: nmax=100000, npmax=10
-	  integer, parameter                 :: neqs=2
-	  real(8), parameter                 :: eps = 1.e-30
-	  real(8)                            :: theta
-	  real(8)                            :: fric,slip
-	  real(8), dimension(neqs)           :: yt, dydt ,yscal, ytsv, dydtsv
-	  real(8)                            :: stiff_temp, fitmis
-	  real(8)                            :: fric_init, stiff_old
-	  real(8)                            :: dx,dt_try, temp1, temp2
-	  real(8)                            :: dt_next, acc, dt_did
-	  real(8)                            :: time, timesv, slipsv
-	  real(8), dimension(:), allocatable :: stiff_vec
-	  integer                            :: i, ioerr, ne, Ncpts, stiff_err
-	  integer, dimension(:), allocatable :: IndCpts, n_stiff
-	  character                          :: ln
-	  character, dimension(29)           :: fm
-	      
-	     
-	    allocate(rnstry_true(size(rnstry)))
-	    call getparams(rnstry,rnstry_true) 
-
-!-------------------------------------------------------------------------------!
-! Initialize
-!-------------------------------------------------------------------------------!
-
-      fitmis      = 0.d0
-      time        = t(1)
-      slipv(1)     = x_f(1)
-      theta       = dc/v_lp(1)
-      fric_init   = aa*log(v_lp(1))+bb*log(theta)
-      yt(2)       = v_lp(1)
-      yt(1)       = theta      
-      acc         = 1.e-8
-      dt_try      = t(2) - t(1)
-      stiff_err   = 0
-      stiff_old   = stiffk
-      tau(1)      = fric_init
-      sliprate(1) = yt(2)
-      state(1)    = yt(1)
-      
-!-------------------------------------------------------------------------------!
-! Time loop
-!-------------------------------------------------------------------------------!
-            do i = 2, Nrange
-		  vo = (x_lp(i)-x_lp(i-1))/(t(i)-t(i-1))
-		  do while (abs(time-t(i)) >= acc)
-		  	  call derivs(time,yt,dydt)
-		  	  yscal(:)=abs(yt(:))+abs(dt_try*dydt(:))
-		  	  ytsv = yt
-		  	  dydtsv = dydt
-		  	  timesv = time
-		  	  slipsv = slip
-		  	  call rkqs(yt,dydt,time,dt_try,acc,yscal,dt_did,dt_next,derivs)
-		  	  if (time.gt.t(i)) then
-		  	  	yt   = ytsv
-		  	  	dydt = dydtsv
-		  	  	time = timesv
-		  	  	dt_try = t(i) - time  
-		  	  else
-		  	  	slipv(i)= slipv(i-1) + yt(2)*dt_did
-		  	  	dt_try = dt_next
-		  	  endif
-          	  end do
-		  state(i)     = yt(1)
-		  sliprate(i)  = yt(2)
-		  tau(i)       = aa*log(sliprate(i))+bb*log(state(i))
-      	   enddo
-      tau = tau - fric_init
-   end subroutine fmodel
    !------------------------------------------------------------------------------------------------!
    !------------------------------------------------------------------------------------------------!
    
    function gauss(x,mu,std)
+   ! Use a standard normal multivariate and convert to a random variable from a
+   ! multivariate normal distribution with mean mu and standard deviation std
    
    real(8)                              :: gauss
    real(8), dimension(:), intent(in)    :: x, mu
@@ -162,9 +31,10 @@ subroutine fmodel(rnstry, tau, slipv, sliprate, state, x_f, x_lp, t, v_lp)
    
    allocate(cinv(size(std,1),size(std,2)))
    cinv=invertmat_sym(std)
-   ! Mahalanobis distance for x
+   ! Mahalanobis distance for vector x
    arg   = -0.5*dot_product(matmul(cinv,x-mu),(x-mu))
    N     = dble(size(x))
+   ! Find the determinant of std
    det_s = det_sym(std)
    denom = ((sqrt(2.d0*pi))**N)*det_s
    gauss = exp(arg)/denom
@@ -174,6 +44,11 @@ subroutine fmodel(rnstry, tau, slipv, sliprate, state, x_f, x_lp, t, v_lp)
    !---------------------------------------------------------------------------------------------------!
    !---------------------------------------------------------------------------------------------------!
    function corr_uni_rand(x,std) result(outvec)
+   
+   ! Correlated uniform random numbers for proposal generation
+   ! Based on the recipe in http://comisef.wikidot.com/tutorial:correlateduniformvariates
+   ! We produce Bravais-Pearson correlation from a pre-specified Spearman correlation
+   ! Then use the normal distribution to map the correlation onto a uniform distribution
    
    real(8), parameter                   :: pi = 4.d0*atan(1.d0)
    real(8), dimension(:), intent(in)    :: x
@@ -215,7 +90,8 @@ subroutine fmodel(rnstry, tau, slipv, sliprate, state, x_f, x_lp, t, v_lp)
  !---------------------------------------------------------------------------------------------------!
  function mix_gauss2(p,mu1,std1,mu2,std2,mix_factor) result(pout)
  !!! Function for generating a parameter proposal from the mixture of two Gaussians with different
- !!! means and std-s according to a predefined probability, mix_factor, to choose either.
+ !!! means and std-s according to a predefined probability, mix_factor, to choose from the first and
+ !!! a probability 1-mix_factor to draw from the second.
  implicit none
  
  real(8), dimension(:), intent(in)       :: p
